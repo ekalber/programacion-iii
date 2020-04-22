c70f6062797073370e4ecbf98cd2746e363fb34e
Evaldo Kalbermatter <ekalber@gmail.com>
2020-04-22 08:23:56 -0300

    Changes until 22-04-2020

--- cars/app/controllers/application_controller.rb
+++ cars/app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  USERS = { "admin" => "admin" }
+ 
+  before_action :authenticate
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+ 
+  private
+ 
+    def authenticate
+      authenticate_or_request_with_http_digest do |username|
+        USERS[username]
+      end
+    end 
+
+    def record_not_found
+      render plain: "404 Not Found", status: 404
+    end 
 end
--- cars/app/models/car.rb
+++ cars/app/models/car.rb
@@ -7,4 +7,20 @@ class Car < ApplicationRecord
 
 	validates :doors, :year, numericality: true
 	validates :color, :model, :brand_id, presence: :true
+
+	validate :doors_and_color
+	validate :relacion_estructura
+
+	def doors_and_color		
+		if self.doors > 5 and self.color == "Blanco"
+			errors.add(:doors, "No puede tener mas de 5 puertas y ser de color blanco")
+		end
+	end
+
+	def relacion_estructura
+		if self.structure.name == "Convertible" and self.doors > 2
+			errors.add(:structure, "No puede ser convertible y tener mas de 2 puertas")
+		end
+	end
+
 end
